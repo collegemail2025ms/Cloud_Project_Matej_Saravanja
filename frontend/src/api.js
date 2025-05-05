e6731d8378ef241b760a2a502a016b164555e0dc
collegemail2025ms <collegemail2025ms@gmail.com>
2025-05-05 11:50:26 +0100

    backend set for html and react

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/products';
+const API_URL = 'https://cloud-project-matej-saravanja.onrender.com/products';
 
 export const getProducts = () => axios.get(API_URL);
 export const getProduct = (id) => axios.get(`${API_URL}/${id}`);
